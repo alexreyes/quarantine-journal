bc6a0e0cdc2f46f08b629d4b57c46810f662d30d
Alex <reyesruizalex@gmail.com>
2020-03-27 14:13:05 -0400

    retrieve posts to UI

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,8 +2,57 @@
 	import Box from './Box.svelte'; 
 	import Content from '../components/Content.svelte';
 	import Modal from '../components/Modal.svelte';	
-	let posts = [];
-	
+	import { arweaveWallet, storedWalletAddress} from '../components/userContext.js';
+    import { and, or, equals } from 'arql-ops';
+
+	let posts = []; 
+
+	// post sample data: 
+	// const newPost = {
+	// 	title : 'hello',
+	// 	description: 'world'
+	// };
+
+	function getPosts() {
+		(async () => {
+			const arweave = Arweave.init();
+
+			const myQuery = and(
+				equals('from', $storedWalletAddress),
+				equals('App-Name', 'QuarantineNotes'),
+				equals('TestData', 'false'),
+				or(
+					equals('App-Version', '0.0.1'),
+				)
+			);
+					
+			const results = await arweave.arql(myQuery);
+			console.log(results); 
+
+			for (var count = 0; count < results.length; count++) {
+												
+				const blockchainTransaction = arweave.transactions.get(results[count]).then(blockchainTransaction => {
+
+					let returnedJson = blockchainTransaction.get('data', {decode: true, string: true});
+					console.log(returnedJson); 
+
+					try {
+						posts = posts.concat(JSON.parse(returnedJson)); // add returned posts to the view
+						
+					} catch (ex) {
+						console.error(ex);
+					}
+					// Get tags
+					blockchainTransaction.get('tags').forEach(tag => {
+						let key = tag.get('name', {decode: true, string: true});
+						let value = tag.get('value', {decode: true, string: true});
+						console.log(`${key} : ${value}`);
+					});
+
+				});
+			}
+        })()
+	}
 </script>
 
 <style>
@@ -30,6 +79,7 @@
 
 <svelte:head>
 	<title>Quarantine Notes</title>
+	<script src="https://unpkg.com/arweave/bundles/web.bundle.js"></script>
 </svelte:head>
 
 <main>
@@ -54,3 +104,5 @@
 <Modal> 
 	<Content/>
 </Modal>
+
+<button on:click="{getPosts}">get posts</button>
--- src/routes/new.svelte
+++ src/routes/new.svelte
@@ -1,8 +1,5 @@
 <script>
 	import { arweaveWallet, storedWalletAddress} from '../components/userContext.js';
-    // import Arweave from 'arweave/web';
-    import { and, or, equals } from 'arql-ops';
-
 
 	let title = ''; 
     let description = ''; 
@@ -45,7 +42,7 @@
 
             transaction.addTag('App-Name', 'QuarantineNotes')
             transaction.addTag('App-Version', '0.0.1')
-            transaction.addTag('TestData', 'true')
+            transaction.addTag('TestData', 'false')
             // transaction.addTag('Unix-Time', unixTime)
 
             await arweave.transactions.sign(transaction, key);
@@ -56,51 +53,6 @@
             await arweave.transactions.post(transaction);
             alert('Note saved to blockchain!');
 
-            const myQuery = and(
-                equals('from', $storedWalletAddress),
-                equals('App-Name', 'QuarantineNotes'),
-                or(
-                    equals('App-Version', '0.0.1'),
-                    equals('TestData', 'true'),
-                )
-            );
-            
-            const results = await arweave.arql(myQuery);
-            console.log(results); 
-
-            for (var count = 0; count < results.length; count++) {
-                                                 
-                const blockchainTransaction = arweave.transactions.get(results[count]).then(blockchainTransaction => {
-
-                    // Use the get method to get a specific transaction field.
-                    // console.log(blockchainTransaction.get('signature'));
-                    // NLiRQSci56KVNk-x86eLT1TyF1ST8pzE-s7jdCJbW-V...
-
-                    // console.log(blockchainTransaction.get('data'));
-                    //CjwhRE9DVFlQRSBodG1sPgo8aHRtbCBsYW5nPSJlbiI-C...
-
-                    // Get the data base64 decoded as a Uint8Array byte array.
-                    // console.log(blockchainTransaction.get('data', {decode: true}));
-                    //Uint8Array[10,60,33,68,79,67,84,89,80,69...
-
-                    // Get the data base64 decoded as a string.
-                    console.log(blockchainTransaction.get('data', {decode: true, string: true}));
-                    //<!DOCTYPE html>
-                    //<html lang="en">
-                    //<head>
-                    //    <meta charset="UTF-8">
-                    //    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    //    <title>ARWEAVE / PEER EXPLORER</title>
-
-                    // blockchainTransaction.get('tags').forEach(tag => {
-                    //     let key = tag.get('name', {decode: true, string: true});
-                    //     let value = tag.get('value', {decode: true, string: true});
-                    //     console.log(`${key} : ${value}`);
-                    // });
-                    // Content-Type : text/html
-                    // User-Agent : ArweaveDeploy/1.1.0
-                });
-            }
         })()
     }
 </script>
